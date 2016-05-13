ee388f1bcb92708ac1d986e92a995c001f5df914
Piocze <piotr.telman@gmail.com>
2016-05-13 23:24:18 +0200

    Added button sending text to controller.
    Next step-creating partial view? and connecting to instagram and
    facebook.

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InFb.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Show(string galleryname)
+        {
+            //Console.WriteLine(galleryname);
+            ViewBag.Message = HttpUtility.HtmlEncode("Witaj " + galleryname);
+
+            return View();
+            
+        }
+    }
+}
