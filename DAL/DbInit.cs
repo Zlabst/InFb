fc471cc1e9fd285ae713702b86f052702a1242bb
Piocze <piotr.telman@gmail.com>
2016-05-13 19:23:47 +0200

    Database added, one table with entries, added entries view

---
+++ DAL/DbInit.cs
@@ -0,0 +1,23 @@
+﻿using InFb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InFb.DAL
+{
+    public class DbInit : System.Data.Entity.DropCreateDatabaseIfModelChanges<EntryContext>
+    {
+        protected override void Seed(EntryContext context)
+        {
+            var entries = new List<Models.Entry>
+            {
+            new Entry{EntryID=1,Name="aaa",Link="bbb"},
+            new Entry{EntryID=2,Name="aaa1",Link="bbb1"}
+            };
+
+            entries.ForEach(s => context.Entries.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Models/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InFb.Models
+{
+    public class Entry
+    {
+        public int EntryID { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
